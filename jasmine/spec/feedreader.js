22f04d04157fd1db02b187470e15702d8b5fcdc4
Bruce Seymour <brucedseymour@gmail.com>
2018-03-22 19:39:34 -0400

    updated new feed selection test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -6,117 +6,116 @@
  *
 */
 
-
 /*
  * COMMENTED CODE IS KEPT INTENTIALLY FOR STUDENT LEARNING PURPOSES.
 */
 
 $(function() {
 
-    describe('RSS Feeds', function() {
-        /*  make sure that the allFeeds variable has been defined and that it is not empty.
+  describe('RSS Feeds', function() {
+    /*  make sure that the allFeeds variable has been defined and that it is not empty.
          */
-        it('are defined', function() {
-            expect(allFeeds).toBeDefined();
-            expect(allFeeds.length).not.toBe(0);
-        });
-
-        // loop through each feed in the allFeeds object and ensure it has a URL defined
-        //  * and that the URL is not empty.
-
-        it('have non-empty URLs', function() {
-          for(var i = 0; i < allFeeds.length; i++) {
-                expect(allFeeds[i].url).toBeDefined();
-                expect(allFeeds[i].url.length).not.toBe(0);
-          }
-        });
-
-        // loop through each feed  in the allFeeds object and ensure it has a name defined
-        // and that the name is not empty.
-
-        it('have a name', function(){
-            allFeeds.forEach(function(feed){
-             // expect(feed.name).toBeDefined();
-             // expect(feed.name.length).not.toBe(0);
-             expect(feed.url).toBeTruthy();
-          });
-        });
-
+    it('are defined', function() {
+      expect(allFeeds).toBeDefined();
+      expect(allFeeds.length).not.toBe(0);
     });
 
+    // loop through each feed in the allFeeds object and ensure it has a URL defined
+    //  * and that the URL is not empty.
 
-    describe('The menu', function() {
+    it('have non-empty URLs', function() {
+      for (var i = 0; i < allFeeds.length; i++) {
+        expect(allFeeds[i].url).toBeDefined();
+        expect(allFeeds[i].url.length).not.toBe(0);
+      }
+    });
 
-      // ensure the menu element is hidden by default.
-      it('is hidden by default', function() {
-          let menuTest = document.querySelector('body.menu-hidden');
-          expect(menuTest).not.toBe(null);
+    // loop through each feed  in the allFeeds object and ensure it has a name defined
+    // and that the name is not empty.
+
+    it('have a name', function() {
+      allFeeds.forEach(function(feed) {
+        // expect(feed.name).toBeDefined();
+        // expect(feed.name.length).not.toBe(0);
+        expect(feed.url).toBeTruthy();
       });
-
-
-
-      // the menu changes visibility when the menu icon is clicked.
-
-      it('changes visibility when menu icon is clicked',
-          function() {
-            let theMenuIconLink = document.querySelector('.menu-icon-link');
-
-            let classCheck1 = document.querySelector('body').classList.contains('menu-hidden');
-            theMenuIconLink.click();
-            expect(classCheck1).toBe(true);
-
-            let classCheck2 = document.querySelector('body').classList.contains('menu-hidden');
-            theMenuIconLink.click();
-            expect(classCheck2).toBe(false);
-          });
     });
 
+  });
+
+  describe('The menu', function() {
+
+    // ensure the menu element is hidden by default.
+    it('is hidden by default', function() {
+      let menuTest = document.querySelector('body.menu-hidden');
+      expect(menuTest).not.toBe(null);
+    });
+
+    // the menu changes visibility when the menu icon is clicked.
 
 
-    describe('Initial Entries', function() {
+    it('changes visibility when menu icon is clicked', function() {
+      let theMenuIconLink = document.querySelector('.menu-icon-link');
 
-      // loadFeed function is called and completes its work,there is at least a single .entry element within the .feed container.
+  //    let classCheck1 = document.querySelector('body').classList.contains('menu-hidden');
+      let classCheck1 = $('body').hasClass('menu-hidden');
+      theMenuIconLink.click();
+      expect(classCheck1).toBe(true);
 
-       beforeEach(function (done){
-         loadFeed(0, function(){
-           done();
-         });
-       });
+  //    let classCheck2 = document.querySelector('body').classList.contains('menu-hidden');
+      let classCheck2 = $('body').hasClass('menu-hidden');
+      theMenuIconLink.click();
+      expect(classCheck2).toBe(false);
+    });
+  });
 
-      it('has a .entry element in the .feed container', function(done){
+  describe('Initial Entries', function() {
 
-      //  $('.parent .child');
-      //  let entryTest = document.querySelector('.entry'); //old
+    // loadFeed function is called and completes its work,there is at least a single .entry element within the .feed container.
+    //  TODO: use .length combined with toBeGreaterThan(0) here.
 
-        let entryTest = $('.feed .entry');
-      //  console.info('EntryTest', entryTest);
-        expect(entryTest).not.toBe(null);
+    beforeEach(function(done) {
+      loadFeed(0, function() {
         done();
       });
-
-
     });
 
-    describe('New Feed Selection', function() {
+    it('has a .entry element in the .feed container', function(done) {
 
-        /* when a new feed is loaded by the loadFeed function that the content actually changes.
-         */
+      //  $('.parent .child');
+      //  let entryTest = document.querySelector('.entry'); old
 
-        beforeEach(function(done) {
-          loadFeed(1, done);
-          myFeed1 = document.querySelectorAll('.entry-link')[0];
-          myFeed1Url = myFeed1.href;
+      let entryTest = $('.feed .entry');
+      //  console.info('EntryTest', entryTest);
+      expect(entryTest).not.toBe(null);
+      done();
+    });
 
-          loadFeed(1, done);
-          myFeed2 = document.querySelectorAll('.entry-link')[1];
-          myFeed2Url = myFeed2.href;
-           });
+  });
 
-       it('has content changed', function(done){
-          console.info('1: ', myFeed1Url, '2:', myFeed2Url);
-          expect(myFeed1Url).not.toBe(myFeed2Url);
+
+
+  describe('New Feed Selection', function() {
+
+    /* when a new feed is loaded by the loadFeed function that the content actually changes. */
+
+    beforeEach(function(done) {
+
+      loadFeed(0, function() {
+        myFeed1Url = document.querySelectorAll('.entry-link')[0].href;
+        loadFeed(1, function() {
+          myFeed2Url = document.querySelectorAll('.entry-link')[0].href;
           done();
         });
+      });
     });
+
+    it('has content changed', function(done) {
+      console.info('1: ', myFeed1Url, '2:', myFeed2Url);
+      expect(myFeed1Url).not.toBe(myFeed2Url);
+      done();
+    });
+
+  });
 
 }());
